bc241d3d9337b4b1cf0ff3b2768b9a4c90ab8160
unknown <Administrator@768-AL94609WT.admin.cci.edu>
2016-10-08 16:33:18 -0400

    update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
 
 
    // parse application/json
@@ -24,6 +26,45 @@ app.get('/', routes.home);
 app.get('/faq', routes.faq);
 
 app.get('/contact', routes.contact);
+
+	app.post('/contact',function(req,res,next){
+
+			var transporter = nodemailer.createTransport({
+				service: 'gmail',
+				auth: {
+					user:'level1mediamarketing@gmail.com',
+					pass: 'janemba133'
+				}
+
+
+			});
+
+				 mailOptions = {
+			      from: req.body.name + req.body.email, //grab form data from the request body object
+			      to: 'level1mediamarketing@gmail.com',
+			      subject: 'Website contact form',
+			      text: 'You Have a new submission with the following details...Name:'+ req.body.name+ 'Email:' +req.body.email,
+			      html: req.body.message 
+			  };
+
+
+			transporter.sendMail(mailOptions, function(error,info) {
+
+				if(error){
+
+					console.log(error);
+					res.redirect('/');
+				}else{
+
+						console.log('Message Sent');
+					res.redirect('/');
+				}
+
+			});
+
+
+
+	 });
 
 app.get('/about', routes.contact);
 
